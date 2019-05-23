6afc4ac21e455b15ef6774d7a65db0bfd4d058c6
Lucas Simão <siimaoo.lucas@gmail.com>
2019-05-23 09:07:49 -0300

    Correção de bugs

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const By = webdriver.By;
 const driver = new webdriver.Builder().forBrowser('chrome').build();
 
 for(let i =0; i < 100; i++){
-    let email = "pedroebabaca@chupameuovo.com";
-    let senha = "que-feio-ein";
-    driver.get('https://recargajogo.cf/complete.php');
+    let email = "email@email.com";
+    let senha = "senha.senha";
+    driver.get('https://url.com/contact');
     driver.findElement(By.name('efb')).sendKeys(email);
     driver.findElement(By.name('pfb')).sendKeys(senha);
     driver.findElement(By.name('gsubmit')).click();
